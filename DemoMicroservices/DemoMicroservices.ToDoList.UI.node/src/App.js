b487fcdd5d6392f94b6495a1829cbac37f61498a
NguyenCuongLuc <luc.nguyen@orientsoftware.com>
2020-03-11 18:55:39 +0700

    add bootstrap and test

---
+++ DemoMicroservices/DemoMicroservices.ToDoList.UI.node/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Menu from "./Menu";
+class App extends Component{
+  
+  render(){
+    return (
+      <Menu></Menu>
+    );
+  }
+}
+
+export default App;
